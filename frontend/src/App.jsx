b35eb1df946540da92b5cb94ad671018c6c5f41c
Harsh <harshchhachhia008@gmail.com>
2025-03-03 23:20:34 +0530

    initialized frontend and backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+    <div className='container mx-auto align-middle text-center'>
+      <h1 className='text-xl font-bold'>Home page</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
