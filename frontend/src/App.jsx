bded196fbba9f0ad68d40ed34547ef655b5c61f3
Nidhi Singh Bhadoria <teamguffonidhi28@gmail.com>
2025-03-05 12:13:51 +0530

    fix: minor update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
     <div className='container mx-auto align-middle text-center'>
-      <h1 className='text-xl font-bold'>Home page</h1>
+      <h1 className='text-xl font-bold'>Home</h1>
     </div>
     </>
   )
