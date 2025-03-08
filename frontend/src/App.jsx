69cb1d9153be079b763dd89e5665e18ba79ac31a
Nidhi Singh Bhadoria <teamguffonidhi28@gmail.com>
2025-03-08 11:28:01 +0530

    feat: UI changes for page 3 and 4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
+import React from "react";
+import Nutrition from "./components/nutrition";
+import WhyUseSuvidhaShare from "./components/whyUseSuvidhaShare";
 import Gallery from "./components/gallery"
 import ClientTestimonials from "./components/clientTestmonials"
 
 function App() {
 
   return (
-    <>  
-    <div>
+    <>
+        < Nutrition />
+        < WhyUseSuvidhaShare />
+     <div>
       <Gallery/>
     </div>
     <div>
