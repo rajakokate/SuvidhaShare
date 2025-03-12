4d6b21e02f6885381c513a5d2e56f103016ad063
coder-devansh <devanshbansal25072004@gmail.com>
2025-03-12 22:51:35 +0530

    [Dashboard done]

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,21 @@
 import Gallery from "./components/gallery"
 import ClientTestimonials from "./components/clientTestmonials"
+import Dashboard from "./components/Dashboard"
 
 function App() {
 
   return (
     <>  
     <div>
+      <Dashboard/>
+    </div>
+    <div>
       <Gallery/>
     </div>
     <div>
       <ClientTestimonials />
     </div>
+
     </>
   )
 }
