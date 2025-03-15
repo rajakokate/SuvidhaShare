fc9120ee05ec15e7dce258d59b5e9711c09a8e6f
coder-devansh <devanshbansal25072004@gmail.com>
2025-03-15 23:04:30 +0530

    [Routing done]

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
-
-
-import Dashboard from "./components/Dashboard"
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Importing necessary components from react-router-dom
+
+// Import your components
+import Dashboard from "./components/Dashboard";
 import Navbar from "./components/navbar";
 import HeroSection from "./components/HeroSection";
 import Solutions from "./components/Solutions";
@@ -12,28 +13,28 @@ import ClientTestimonials from "./components/clientTestmonials";
 
 const App = () => {
   return (
-    <>
-    <div>
-      <Dashboard/>
-    </div>
-     <div>
-     <Navbar />
-      </div>
-     <HeroSection />
-     <Solutions />
-     <Nutrition />
-     <WhyUseSuvidhaShare />
-    <div>
-      <Gallery/>
-    </div>
-    <div>
-      <ClientTestimonials />
-    </div>
+    <Router> {/* Wrapping the app with Router to enable routing */}
+      {/* Navbar will be available on all pages */}
 
-    </>
-  )
-}
-     
+      <Routes> {/* Only one Route will be rendered at a time */}
+        {/* Route for Dashboard */}
+        <Route path="/dashboard" element={<Dashboard />} />
 
+        {/* Default Route (Home page) */}
+        <Route path="/" element={
+          <>
+           <Navbar />
+            <HeroSection />
+            <Solutions />
+            <Nutrition />
+            <WhyUseSuvidhaShare />
+            <Gallery />
+            <ClientTestimonials />
+          </>
+        } />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
