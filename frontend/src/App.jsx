1878138ccdd086169f414dd7cc0b8cadfe3bedf5
Gayatri Rasala <gayatrirasala1@gmail.com>
2025-03-15 10:13:37 +0530

    Added SignUp component and updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Navbar from "./components/navbar";
 import HeroSection from "./components/HeroSection";
 import Solutions from "./components/Solutions";
-
+import SignUp from "./components/SignUp";
 const App = () => {
   return (
     <div>
       <Navbar />
       <HeroSection />
       <Solutions />
+      <SignUp/>
     </div>
   );
 };
