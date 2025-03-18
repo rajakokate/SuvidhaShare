fc6f835c15fa729342f8fad48cb8c25e73b4b961
Harsh <harshchhachhia008@gmail.com>
2025-03-18 23:44:46 +0530

    connected to database and added authentication

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
-import express from "express"
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-const app = express()
+dotenv.config();
 
-app.get("/", (req, res) => {
-    res.send("Hello World")
-    } 
-)
-
-app.listen(5000, () => {
-    console.log("Server started at http://localhost:5000")
-    }
-)
+connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("Express error: ", error);
+            throw error;
+        });
+        app.listen(process.env.PORT || 5000, () => {
+            console.log(
+                `Server is running on port ${process.env.PORT || 5000}`
+            );
+        });
+    })
+    .catch((err) => {
+        console.log("MongoDB connection failed: ", err);
+    });
