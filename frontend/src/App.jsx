b935001afb24c59edba71b3ca69baaad1da98e84
Nidhi Singh Bhadoria <teamguffonidhi28@gmail.com>
2025-03-28 22:29:12 +0530

    alllistfodn function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import "./axiosConfig.js";
 import Dashboard from "./components/Dashboard";
 import Navbar from "./components/navbar";
 import HeroSection from "./components/HeroSection";
-import Solutions from "./components/Solutions"
+import Solutions from "./components/Solutions";
 import Nutrition from "./components/nutrition";
 import WhyUseSuvidhaShare from "./components/whyUseSuvidhaShare";
 import Gallery from "./components/gallery";
@@ -18,6 +18,7 @@ import AboutUs from "./pages/AboutUs";
 import SignIn from "./components/signin";
 import SignUp from "./components/SignUp";
 import AddFood from "./components/AddFoodList";
+import AllFoodLists from "./pages/AllFoodLists.jsx";
 
 const App = () => {
   return (
@@ -25,11 +26,14 @@ const App = () => {
       {" "}
       {/* Wrapping the app with Router to enable routing */}
       {/* Navbar will be available on all pages */}
-      <Routes> {/* Only one Route will be rendered at a time */}
+      <Routes>
+        {" "}
+        {/* Only one Route will be rendered at a time */}
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
-        <Route path="/aboutus" element={<AboutUs />}/>
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/allfoodlists" element={<AllFoodLists />} />
         {/* Default Route (Home page) */}
         <Route
           path="/"
@@ -37,13 +41,13 @@ const App = () => {
             <>
               <Navbar />
               <HeroSection />
-              <AddFood/>
+              <AddFood />
               <Solutions />
               <Nutrition />
               <WhyUseSuvidhaShare />
               <Gallery />
               <ClientTestimonials />
-              <ContactUs/>
+              <ContactUs />
             </>
           }
         />
