a8a1e8915270c04ef876c350c9362f0a372394d9
Rahul Bhardwaj <rahulbhardwaj101031@gmail.com>
2025-04-08 01:47:21 +0530

    added get_user_foods_route

--- backend/routes/food.routes.js
+++ backend/routes/food.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { addFood } from "../controllers/food.controller.js";
+import { addFood, getUserFoods } from "../controllers/food.controller.js";
 
 const router = Router();
 
+// Route to add a food donation
 router.route("/add").post(verifyJWT, addFood);
+
+// Route to get all foods by the logged-in user
+router.route("/my-foods").get(verifyJWT, getUserFoods);
 
 export default router;
