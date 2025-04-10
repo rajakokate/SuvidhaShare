88ff309ede2e785f61a6f715274c2c45507f507b
rajakokate <rajakokate420@gmail.com>
2025-04-10 22:34:44 +0530

    changed file name to SignIn.jsx, to resolve verccel issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ import Gallery from "./components/gallery";
 import ClientTestimonials from "./components/clientTestmonials";
 import ContactUs from "./components/contactUs";
 import AboutUs from "./pages/AboutUs";
-import SignIn from "./components/signin";
+import SignIn from "./components/Signin.jsx";
 import SignUp from "./components/SignUp";
 import AddFood from "./components/AddFoodList";
 import AllFoodLists from "./pages/AllFoodLists.jsx";
